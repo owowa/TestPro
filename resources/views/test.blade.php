ecff7f8f5491e9c73c67166857e55fe902392dca
owowow <43901551+owowa@users.noreply.github.com>
2019-11-21 16:32:47 +0900

    Ajax Test
    ajax로 팝업창 띄우기

---
+++ resources/views/test.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>Ajax 왜 안되</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+ 
+ <style>
+   .container{
+    padding: 0.5%;
+   } 
+</style>
+</head>
+<body>
+ 
+<div class="container">
+    <button>confirm</button>
+</div>
+</body>
+
+<script>
+  $(document).ready(function () {
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+ 
+   //delete user login
+    $('button').on('click', function () {
+        if(confirm("Ajax Test")) {
+ 
+       }
+    });   
+  });
+</script>
+
+</html>
